06dea845ca2d47d56cb50fde891df96637041e50
andrea-minh <94523704+andrea-minh@users.noreply.github.com>
2021-11-17 15:52:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
+    titleWeb: "Gửi crush thân iu",
     introTitle: 'Crush yêu dấu',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tôi lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    btnIntro: 'Hihi >W<',
+    title: 'Phải chăng ai đó đã yêu tôi ngay từ cái nhìn đầu tiên :3',
     desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Tớ cũng thích cậu',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Lần tới bao mình ăn nhaa',
+    btnAccept: 'Okiii lun <3',
     messLink: 'https://m.me' //link mess của các bạn. VD: /dinhkaito
 }
