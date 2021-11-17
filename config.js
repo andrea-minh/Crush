d9e4bf89d29371742e50eb1d7fb85e414bd4e78e
andrea-minh <94523704+andrea-minh@users.noreply.github.com>
2021-11-17 16:50:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
-    introTitle: 'Crush yêu dấu',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "Gửi bestie",
+    introTitle: 'Bestie yêu dấu',
+    introDesc: `Cảm ơn đã luôn chấp nhận và bao nuôi đứa bạn này
+Yew m nhìu`,
+    btnIntro: 'T cũng vậy!',
+    title: 'Phải chăng hết dịch m sẽ bao t đi ăn? <33',
+    desc: 'Thoát web là chấp nhận rồi nhá!!!',
+    btnYes: 'Ok, Bao thì bao',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me' //link mess của các bạn. VD: /dinhkaito
+    question: 'Trên thế giới hơn 7 tỉ người và trái tym của chúng ta là chung 1 nhịp bae à. Vậy ngày nào đi dọ? <3',
+    btnReply: 'Gửi cho Minh',
+    reply: 'Sau dịch thì đi ngay và luôn.',
+    mess: 'T biết mà, yêu m 😘',
+    messDesc: 'Tối call t nha',
+    btnAccept: 'Ok',
+    messLink: 'https://www.facebook.com/'
 }
